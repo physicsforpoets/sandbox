6a6f12a455f88c7a4f02ecf4b3575249187a15b4
Ryan Clark <ryan.clark@showtime.net>
2015-07-30 17:03:22 -0400

    Created base form view; added 'new' form

--- marionette-contacts/assets/js/apps/contacts/list/list_controller.js
+++ marionette-contacts/assets/js/apps/contacts/list/list_controller.js
@@ -17,7 +17,35 @@ ContactManager.module('ContactsApp.List', function(List, ContactManager, Backbon
 				contactsListLayout.on('show', function(){
 					contactsListLayout.panelRegion.show(contactsListPanel);
 					contactsListLayout.contactsRegion.show(contactsListView);
-				})
+				});
+
+				contactsListPanel.on('contact:new', function(){
+					var newContact = new ContactManager.Entities.Contact();
+
+					var view = new ContactManager.ContactsApp.New.Contact({
+						model: newContact,
+						asModal: true
+					});
+
+					view.on('form:submit', function(data){
+						if(contacts.length > 0){
+							var highestId = contacts.max(function(c){ return c.id; }).get('id');
+							data.id = highestId + 1;
+						} else {
+							data.id = 1;
+						}
+
+						if(newContact.save(data)){
+							contacts.add(newContact);
+							ContactManager.dialogRegion.empty();
+							contactsListView.children.findByModel(newContact).flash('success');
+						} else {
+							view.triggerMethod('form:data:invalid', newContact.validationError);
+						}
+					});
+
+					ContactManager.regions.dialog.show(view);
+				});
 
 				contactsListView.on('childview:contact:show', function(childView, model){
 					ContactManager.trigger('contact:show', model.get('id'));
---
+++ marionette-contacts/assets/js/apps/contacts/new/new_view.js
@@ -0,0 +1,5 @@
+ContactManager.module('ContactsApp.Edit', function(Edit, ContactManager, Backbone, Marionette, $, _){
+	New.Contact = ContactManager.ContactsApp.Common.Views.Form.extend({
+		title: 'New Contact'
+	});
+});
